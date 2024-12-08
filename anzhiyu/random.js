ab109eaa24b08b4d4e2201eed5c24abbc2313df7
light222209 <95774265@qq.com>
2024-12-08 18:35:19 +0800

    Site updated: 2024-12-08 18:35:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/10/15/FileBrowser搭建轻量级文件服务器/","2023/10/15/Hexo基础环境搭建/","2023/10/29/Markdown常用语法/","2024/12/08/NPT服务搭建与配制/","2023/10/29/OpenWrt手动安装PassWall插件/","2024/11/17/Samba网络文件服务/","2023/10/15/安装Butterfly主题/","2023/11/02/安装部署docker docker-compse/","2023/11/13/激活Win10-Win11-Office/"];function toRandomPost(){
+var posts=["2023/10/15/FileBrowser搭建轻量级文件服务器/","2023/10/15/Hexo基础环境搭建/","2023/10/29/Markdown常用语法/","2023/10/29/OpenWrt手动安装PassWall插件/","2024/12/08/NPT服务搭建与配制/","2024/11/17/Samba网络文件服务/","2023/10/15/安装Butterfly主题/","2023/11/13/激活Win10-Win11-Office/","2023/11/02/安装部署docker docker-compse/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
