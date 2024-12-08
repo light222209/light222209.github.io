0a02064b5aad9410d27deae11b79c4f5eacc5c72
light222209 <95774265@qq.com>
2024-12-08 14:00:22 +0800

    Site updated: 2024-12-08 14:00:11

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2024/11/27/hello-world/","2023/10/15/FileBrowser搭建轻量级文件服务器/","2023/10/15/Hexo基础环境搭建/","2023/10/29/OpenWrt手动安装PassWall插件/","2023/10/29/Markdown常用语法/","2024/11/17/Samba网络文件服务/","2023/10/15/安装Butterfly主题/","2023/11/02/安装部署docker docker-compse/","2024/05/05/Excel函数/","2023/11/13/激活Win10-Win11-Office/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
